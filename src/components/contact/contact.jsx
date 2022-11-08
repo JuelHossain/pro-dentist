dd61831f51f01bf29d2b65f05df3990976d3f404
juelhossain <juelhossainnn@gmail.com>
2022-11-08 22:00:05 +0600

    contact us section done

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,122 +1,130 @@
-import { ActionIcon, Button, createStyles, Group, SimpleGrid, Text, Textarea, TextInput, Title } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { randomId } from "@mantine/hooks";
-import { showNotification } from "@mantine/notifications";
-import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from "@tabler/icons";
-import { useMatch } from "react-router-dom";
+import { Button, createStyles, Group, Paper, SimpleGrid, Text, Textarea, TextInput } from "@mantine/core";
+import bg from "./bg.svg";
 import { ContactIconsList } from "./ContactIcons";
 
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    minHeight: 400,
-    boxSizing: "border-box",
-    backgroundImage: `linear-gradient(-60deg, ${theme.colors[theme.primaryColor][4]} 0%, ${
-      theme.colors[theme.primaryColor][7]
-    } 100%)`,
-    borderRadius: theme.radius.md,
-    padding: theme.spacing.xl * 2.5,
+const useStyles = createStyles((theme) => {
+  const BREAKPOINT = theme.fn.smallerThan("sm");
 
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      padding: theme.spacing.xl * 1.5,
+  return {
+    wrapper: {
+      display: "flex",
+      backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+      borderRadius: theme.radius.lg,
+      padding: 4,
+      border: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[2]}`,
+
+      [BREAKPOINT]: {
+        flexDirection: "column",
+      },
     },
-  },
 
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    color: theme.white,
-    lineHeight: 1,
-  },
+    form: {
+      boxSizing: "border-box",
+      flex: 1,
+      padding: theme.spacing.xl,
+      paddingLeft: theme.spacing.xl * 2,
+      borderLeft: 0,
 
-  description: {
-    color: theme.colors[theme.primaryColor][0],
-    maxWidth: 300,
-
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      maxWidth: "100%",
+      [BREAKPOINT]: {
+        padding: theme.spacing.md,
+        paddingLeft: theme.spacing.md,
+      },
     },
-  },
 
-  form: {
-    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-    padding: theme.spacing.xl,
-    borderRadius: theme.radius.md,
-    boxShadow: theme.shadows.lg,
-  },
-
-  social: {
-    color: theme.white,
-
-    "&:hover": {
-      color: theme.colors[theme.primaryColor][1],
+    fields: {
+      marginTop: -12,
     },
-  },
 
-  control: {
-    backgroundColor: theme.colors[theme.primaryColor][6],
-  },
-}));
+    fieldInput: {
+      flex: 1,
 
-const social = [IconBrandTwitter, IconBrandYoutube, IconBrandInstagram];
+      "& + &": {
+        marginLeft: theme.spacing.md,
 
-export default function ContactUs() {
-  const { classes } = useStyles();
-  const matches = useMatch("/contact");
-
-  const icons = social.map((Icon) => (
-    <ActionIcon key={randomId()} size={28} className={classes.social} variant="transparent">
-      <Icon size={22} stroke={1.5} />
-    </ActionIcon>
-  ));
-
-  const { onSubmit, getInputProps, reset } = useForm({
-    initialValues: {
-      email: "",
-      name: "",
-      message: "",
+        [BREAKPOINT]: {
+          marginLeft: 0,
+          marginTop: theme.spacing.md,
+        },
+      },
     },
-  });
 
-  const submitHandler = (e) => {
-    onSubmit(() => {
-      showNotification({
-        title: "Your Message Has Been Sent",
-        message: "We Will contact You very soon",
-      });
-      reset();
-    })(e);
+    fieldsGroup: {
+      display: "flex",
+
+      [BREAKPOINT]: {
+        flexDirection: "column",
+      },
+    },
+
+    contacts: {
+      boxSizing: "border-box",
+      position: "relative",
+      borderRadius: theme.radius.lg - 2,
+      backgroundColor:theme.colors.main[6],
+      border: "1px solid transparent",
+      padding: theme.spacing.xl,
+      flex: "0 0 280px",
+
+      [BREAKPOINT]: {
+        marginBottom: theme.spacing.sm,
+        paddingLeft: theme.spacing.md,
+      },
+    },
+
+    title: {
+      marginBottom: theme.spacing.xl * 1.5,
+      fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+      [BREAKPOINT]: {
+        marginBottom: theme.spacing.xl,
+      },
+    },
+
+    control: {
+      [BREAKPOINT]: {
+        flex: 1,
+      },
+    },
   };
+});
+
+export default function Contact() {
+  const { classes } = useStyles();
+
   return (
-    <div className={`${classes.wrapper} ${matches && "mt-10 md:mt-20"}`}>
-      <SimpleGrid cols={2} spacing={50} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
-        <div>
-          <Title className={classes.title}>Contact us</Title>
-          <Text className={classes.description} mt="sm" mb={30}>
-            Leave your email and we will get back to you within 24 hours
+    <Paper shadow="md" radius="lg">
+      <div className={classes.wrapper}>
+        <div className={classes.contacts}>
+          <Text size="lg" weight={700} className={classes.title} sx={{ color: "#fff" }}>
+            Contact information
           </Text>
 
           <ContactIconsList variant="white" />
-
-          <Group mt="xl">{icons}</Group>
         </div>
-        <form className={classes.form} onSubmit={submitHandler}>
-          <TextInput label="Email" placeholder="your@email.com" required {...getInputProps("email")} type="email" />
-          <TextInput label="Name" placeholder="John Doe" mt="md" {...getInputProps("name")} />
-          <Textarea
-            required
-            label="Your message"
-            placeholder="I want to enroll in a course"
-            minRows={4}
-            mt="md"
-            {...getInputProps("message")}
-          />
 
-          <Group position="right" mt="md">
-            <Button type="submit" className={classes.control}>
-              Send message
-            </Button>
-          </Group>
+        <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
+          <Text size="lg" weight={700} className={classes.title}>
+            Get in touch
+          </Text>
+
+          <div className={classes.fields}>
+            <SimpleGrid cols={2} breakpoints={[{ maxWidth: "sm", cols: 1 }]}>
+              <TextInput label="Your name" placeholder="Your name" />
+              <TextInput label="Your email" placeholder="hello@mantine.dev" required />
+            </SimpleGrid>
+
+            <TextInput mt="md" label="Subject" placeholder="Subject" required />
+
+            <Textarea mt="md" label="Your message" placeholder="Please include all relevant information" minRows={3} />
+
+            <Group position="right" mt="md">
+              <Button type="submit" className={classes.control}>
+                Send message
+              </Button>
+            </Group>
+          </div>
         </form>
-      </SimpleGrid>
-    </div>
+      </div>
+    </Paper>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mantine/core";
+import Contact from "../../components/contact/contact";
 import Banner from "./Banner";
 
 export default function Home() {
   return (
     <Box className=" my-5 md:my-10 space-y-10 md:space-y-20">
       <Banner />
+      <Contact />
     </Box>
   );
 }
